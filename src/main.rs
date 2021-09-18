d411670f287e2ae2c6d715d08ef3d24cbdb34345
Riccardo Casatta <riccardo@casatta.it>
2021-09-18 16:33:59 +0200

    remove pushbytes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use blocks_iterator::{periodic_log_level, Config};
+use bitcoin::blockdata::opcodes::all::OP_RETURN;
+use bitcoin::blockdata::script::Instruction;
+use bitcoin::{Script, Txid};
+use blocks_iterator::Config;
 use env_logger::Env;
-use log::{info, log, warn};
-use std::collections::HashMap;
-use std::error::Error;
-use std::sync::mpsc::sync_channel;
+use log::info;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::sync::mpsc::{sync_channel, RecvError};
 use structopt::StructOpt;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+enum Error {
+    Recv(RecvError),
+}
+
+impl From<RecvError> for Error {
+    fn from(r: RecvError) -> Self {
+        Error::Recv(r)
+    }
+}
+
+fn main() -> Result<(), Error> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     info!("start");
-
-    let mut map = HashMap::new();
-
     let mut config = Config::from_args();
     config.skip_prevout = true;
     let (send, recv) = sync_channel(100);
@@ -20,19 +32,64 @@ fn main() -> Result<(), Box<dyn Error>> {
         for tx in block_extra.block.txdata.iter() {
             for output in tx.output.iter() {
                 if output.script_pubkey.is_op_return() {
-                    if let Ok(str) = std::str::from_utf8(&output.script_pubkey.as_bytes()[1..]) {
-                        map.insert(tx.txid(), (block_extra.block.header.time, str.to_string()));
+                    if let Some(str) = str_from_op_return(&output.script_pubkey) {
+                        let txid = tx.txid();
+                        let page_dirname = page_dirname(&txid);
+                        if !page_dirname.exists() {
+                            std::fs::create_dir_all(&page_dirname).unwrap();
+                            let mut page_filename = page_dirname;
+                            page_filename.push("index.html");
+                            create_page(page_filename, &txid, block_extra.block.header.time, str);
+                        }
                     }
                 }
             }
         }
-        log!(
-            periodic_log_level(block_extra.height),
-            "map size: {}",
-            map.len()
-        )
     }
     handle.join().expect("couldn't join");
     info!("end");
     Ok(())
+}
+
+fn page_dirname(txid: &Txid) -> PathBuf {
+    let mut path = PathBuf::new();
+    path.push("_site");
+    path.push("m");
+    path.push(txid.to_string());
+    path
+}
+
+fn create_page(filename: PathBuf, txid: &Txid, time: u32, msg: &str) {
+    let mut file = File::create(filename).unwrap();
+    let page = format!("<!DOCTYPE html><html><head><meta charset=\"utf-8\"/></head><body><p>txid: {}</p><p>timestamp: {}</p><p>message: {}</p></body></html>", txid, time, msg);
+    file.write(page.as_bytes()).unwrap();
+}
+
+fn str_from_op_return(script: &Script) -> Option<&str> {
+    let mut instructions = script.instructions();
+    if let Some(Ok(Instruction::Op(all))) = instructions.next() {
+        if all == OP_RETURN {
+            if let Some(Ok(Instruction::PushBytes(bytes))) = instructions.next() {
+                return Some(std::str::from_utf8(bytes).ok()?);
+            }
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use bitcoin::consensus::deserialize;
+    use bitcoin::hashes::hex::FromHex;
+    use bitcoin::Script;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_parsing() {
+        // op_return script in tx 0e20ae6ed9d1de7eb84823bfb4445fc3421e489c31d7694693b2fecb7d184807
+        let script = Script::from_str("6a1645574275696c64696e67207468652077616c6c2e2e2e").unwrap();
+        for ins in script.instructions() {
+            println!("{:?}", ins);
+        }
+    }
 }
