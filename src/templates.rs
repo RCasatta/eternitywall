509f7d094e4cf594e90e0ce04becb8f0420ef32f
Riccardo Casatta <riccardo@casatta.it>
2021-09-26 08:10:04 +0200

    add viewport meta

--- src/templates.rs
+++ src/templates.rs
@@ -8,6 +8,8 @@ fn header() -> Markup {
     html! {
         head {
             meta charset="utf-8";
+            meta name="viewport" content="width=device-width, initial-scale=1.0";
+
             title { "EternityWall" }
         }
     }
